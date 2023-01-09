876de120b5be0d1bfb6fc05db830bf01c87c300f
pkarisa <faith.pendo@student.moringaschool.com>
2023-01-09 22:38:29 +0300

    code change

--- app.js
+++ app.js
@@ -7,6 +7,26 @@ app.get('/', (request,response)=>{
    response.render('index');
 })
 
+// Import express package
+const express = require('express');
+const upload = require('./upload');
+
+app.get('/', (request,response)=>{
+   response.render('index');
+})
+
+// route to handle image upload
+app.post('/upload', (request,response)=>{
+   upload(request,response, (err)=>{
+       if (err){
+           console.log(err)
+       }else{
+           console.log(request.file)
+           response.send('test');
+       }
+   })
+})
+
 
 // Define the port number
 const PORT = 5000;
